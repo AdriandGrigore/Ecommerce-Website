c68f0abe2426c19e1fb3da64f6a8c2d00bed5585
AdriandGrigore <grigorenicolaeadrian@yahoo.com>
2023-07-03 16:07:16 +0300

    page scrolls to top after switching routes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home';
 import { Route, Routes } from 'react-router-dom';
 import Shop from './pages/Shop';
+import ScrollToTop from './routes/ScrollToTop';
 
 function App() {
   return (
     <div className="App">
+      <ScrollToTop />
       <Routes>
         <Route
           path='/'
