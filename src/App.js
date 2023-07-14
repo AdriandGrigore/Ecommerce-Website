47c1f09de8effc159cb723f9a84b005f9d4503ed
AdriandGrigore <grigorenicolaeadrian@yahoo.com>
2023-07-14 18:02:47 +0300

    condition removed on ProductModal component

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="App">
       <ScrollToTop />
-      {modalStatus ? <ProductModal /> : null}
+      <ProductModal />
       <Routes>
         <Route
           path='/'
