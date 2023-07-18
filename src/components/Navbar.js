62c4638299da66cf29809ba21ba3c0d68b7b7aaa
AdriandGrigore <grigorenicolaeadrian@yahoo.com>
2023-07-18 09:11:58 +0300

    cart icon from navbar onClick shows the CartOffcanvas component + background color changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Container, Image, Nav } from 'react-bootstrap'
 import NavBar from 'react-bootstrap/Navbar'
 import logo from '../assets/MangataLogo.png'
@@ -6,10 +6,13 @@ import { AiOutlineShoppingCart, AiOutlineHeart } from 'react-icons/ai'
 import { CgProfile } from 'react-icons/cg'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import CartOffcanvas from './CartOffcanvas'
 import "../css/Navbar.css"
 
 function Navbar() {
     const { cartItems } = useSelector(state => state.cart)
+    const [showCartOffcanvas, setShowOffcanvas] = useState(false)
+
     const navbarLinks = [
         {
             name: "Home",
@@ -34,85 +37,91 @@ function Navbar() {
     ]
 
     return (
-        <NavBar
-            style={{ backgroundColor: "white" }}
-            expand="md"
-            sticky='top'
-            className='pb-0 border border-start-0 border-end-0 border-top-0'
-        >
-            <Container
-                className='py-2 flex-wrap'
+        <>
+            <CartOffcanvas show={showCartOffcanvas} hide={() => setShowOffcanvas(false)} />
+            <NavBar
+                style={{ backgroundColor: "white" }}
+                expand="md"
+                sticky='top'
+                className='pb-0 border border-start-0 border-end-0 border-top-0'
             >
-                <NavBar.Brand
-                    href="/#"
+                <Container
+                    className='py-2 flex-wrap'
                 >
-                    <Image
-                        src={logo}
-                        alt='logo'
-                        style={{ width: "100px" }}
-                    />
-                </NavBar.Brand>
-                <NavBar.Toggle aria-controls="responsive-navbar-nav" />
-                <NavBar.Collapse
-                    id="responsive-navbar-nav"
-                >
-                    <Nav
-                        className="ms-0 ms-md-3 fs-5"
+                    <NavBar.Brand
+                        href="/#"
                     >
-                        {
-                            navbarLinks.map((link, index) => {
-                                return (
-                                    <Link
-                                        key={index}
-                                        to={link.href}
-                                        className='px-3 py-3 py-md-2 navbar-links fs-5 me-0 me-md-2'
-                                    >
-                                        {link.name}
-                                    </Link>
-                                )
-                            })
-                        }
-                    </Nav>
-                </NavBar.Collapse>
-                <div
-                    className='col-12 col-md-auto mx-auto mt-3'
-                >
-                    <Button
-                        variant='ligth'
-                        className='position-relative'
-                    >
-                        <AiOutlineShoppingCart
-                            className='text-secondary fs-3'
+                        <Image
+                            src={logo}
+                            alt='logo'
+                            style={{ width: "100px" }}
                         />
-                        <span
-                            className='position-absolute top-25 start-75 translate-middle text-white bg-primary px-2 rounded-circle'
+                    </NavBar.Brand>
+                    <NavBar.Toggle aria-controls="responsive-navbar-nav" />
+                    <NavBar.Collapse
+                        id="responsive-navbar-nav"
+                    >
+                        <Nav
+                            className="ms-0 ms-md-3 fs-5"
                         >
-                            {cartItems.length}
-                        </span>
-                    </Button>
-                    <Button
-                        variant='ligth'
-                        className='mx-4 mx-md-3 position-relative'
+                            {
+                                navbarLinks.map((link, index) => {
+                                    return (
+                                        <Link
+                                            key={index}
+                                            to={link.href}
+                                            className='px-3 py-3 py-md-2 navbar-links fs-5 me-0 me-md-2'
+                                        >
+                                            {link.name}
+                                        </Link>
+                                    )
+                                })
+                            }
+                        </Nav>
+                    </NavBar.Collapse>
+                    <div
+                        className='col-12 col-md-auto mx-auto mt-3'
                     >
-                        <AiOutlineHeart
-                            className='text-secondary fs-3'
-                        />
-                        <span
-                            className='position-absolute top-25 start-75 translate-middle text-white bg-primary px-2 rounded-circle'
+                        <Button
+                            variant='ligth'
+                            className='position-relative'
+                            onClick={() => setShowOffcanvas(true)}
                         >
-                            0
-                        </span>
-                    </Button>
-                    <Button
-                        variant='ligth'
-                    >
-                        <CgProfile
-                            className='text-secondary fs-3'
-                        />
-                    </Button>
-                </div>
-            </Container>
-        </NavBar>
+                            <AiOutlineShoppingCart
+                                className='text-secondary fs-3'
+                            />
+                            <span
+                                className='position-absolute top-25 start-75 translate-middle text-white px-2 rounded-circle'
+                                style={{ backgroundColor: "rgb(2, 84, 100)" }}
+                            >
+                                {cartItems.length}
+                            </span>
+                        </Button>
+                        <Button
+                            variant='ligth'
+                            className='mx-4 mx-md-3 position-relative'
+                        >
+                            <AiOutlineHeart
+                                className='text-secondary fs-3'
+                            />
+                            <span
+                                className='position-absolute top-25 start-75 translate-middle text-white px-2 rounded-circle'
+                                style={{ backgroundColor: "rgb(2, 84, 100)" }}
+                            >
+                                0
+                            </span>
+                        </Button>
+                        <Button
+                            variant='ligth'
+                        >
+                            <CgProfile
+                                className='text-secondary fs-3'
+                            />
+                        </Button>
+                    </div>
+                </Container>
+            </NavBar>
+        </>
     )
 }
 
